7267e6c407b461bcd622e15249dd1efc67a277b6
HANKWRH <91002606+HANKWRH@users.noreply.github.com>
2022-05-03 18:00:58 +0800

    Update MatrixConversion.py

--- MatrixConversion.py
+++ MatrixConversion.py
@@ -1,24 +1,38 @@
-b = [int(x) for x in input().split()]
-ROWS = b[0]
-COLS = b[1]
-x1 = []
-x2 = []
-def rotate():
-    for i in range(ROWS):
-        pass
+from sys import stdin
+from collections import deque
+lines = deque(stdin.readlines())
+
+while lines:
     
+    
+    ROWS, COLS, _ = [int(x) for x in lines.popleft().split()]
+    
+    x1 = []
 
-for i in range(ROWS):
-    a = [int(x) for x in input().split()]
-    x1.append(a)
-x1.reverse()
-print(x1)
-
-for i in range(b[2]):
-    c = [int(x) for x in input().split()]
-    x2.append(c)
-for i in range(len(x2)):
-    if x2[i] == 1:
-        x1.reverse()
-    else:
-        x1.rotate()
+    def rotate(x):
+        result = []
+        ROWS = len(x)
+        COLS = len(x[0])
+        for j in range(COLS-1,-1,-1):
+            row = []
+            for i in range(ROWS):
+                row.append(x[i][j])
+            result.append(row)
+        return result
+        
+    
+    for i in range(ROWS):
+        a = [int(x) for x in lines.popleft().split()]
+        x1.append(a)
+    
+    x2 = [int(x) for x in lines.popleft().split()]
+    x2.reverse()
+    
+    for x in x2:
+        if x == 1:
+            x1.reverse()
+        else:
+            x1=rotate(x1)
+    print(len(x1),len(x1[0]))
+    for row in x1:
+        print(*row)
