4a16fa28d0133f52bf3b7defcbd0bd144903df1c
HANKWRH <91002606+HANKWRH@users.noreply.github.com>
2022-05-15 12:28:32 +0800

    Update fence.py

--- fence.py
+++ fence.py
@@ -1,15 +1,13 @@
-fence = [int(x) for x in input()]
+_ = [int(x) for x in input()]
 a = [int(x) for x in input().split()]
-c = 0
 list_b = []
 for i in range(len(a)):
-    if  i > 0 and i < (len(a)-1):
-        if a[i] == 0:
+    if a[i] == 0:
+        if  i > 0 and i < (len(a)-1):
             list_b.append(min(a[i-1],a[i+1]))
-    elif i == 0:
-        list_b.append(a[i+1])
-    else:
-        list_b.append(a[i-1])
-for i in range(len(list_b)):
-    c += list_b[i]
-print(c)
+        elif i == 0:
+            list_b.append(a[i+1])
+        else:
+            list_b.append(a[i-1])
+
+print(sum(list_b))
